58884f6309527dead0b1eb6767d0bd36776e8400
paul morin & jennifer bolanos <student@epicodus.com>
2015-04-30 11:24:29 -0700

    list phone number by type

--- spec/phone_spec.rb
+++ spec/phone_spec.rb
@@ -10,11 +10,18 @@ describe(Phone)do
     end
   end
 
-  describe('digits') do
+  describe('#digits') do
     it ("returns the seven figure number phone number") do
       test_phone = Phone.new({:area => "503", :digits => "111-2222", :type => "cell"})
       expect(test_phone.digits()).to(eq("111-2222"))
-    end  
+    end
+  end
+
+  describe('#type') do
+    it("defines the type of phone number") do
+      test_phone = Phone.new({:area => "503", :digits => "111-2222", :type => "cell"})
+      expect(test_phone.type()).to(eq("cell"))
+    end
   end
 
 
