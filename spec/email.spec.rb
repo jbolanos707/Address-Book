9a2128b5e141ff094a5219457efbb97ee93b4c4d
paul morin & jennifer bolanos <student@epicodus.com>
2015-04-30 12:11:11 -0700

    specify whether the email is work or personal

--- spec/email.spec.rb
+++ spec/email.spec.rb
@@ -9,3 +9,10 @@ describe(Email) do
     expect(test_contact.mail()).to(eq("jo@yahoo.com"))
   end
 end
+
+  describe('#type')
+  it("specifies whether the email is work or personal")
+    test_email = Email.new({:mail => "jo@yahoo.com", :type => "work"})
+    expect(test_contact.type()).to(eq("work"))
+  end
+end
